26602300ab5775b01d7c08c02bf93f8ca3d805fd
mistretandon <enggparveshtandon@gmail.com>
2022-10-27 17:26:04 +0530

    feat: onSuccess, onError callback configuration

--- reactjs-query-demo/src/components/SuperHeroes.page.js
+++ reactjs-query-demo/src/components/SuperHeroes.page.js
@@ -1,11 +1,49 @@
 // SuperHeroesPage
-import React from 'react';
+import React from "react";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchSuperHeroes = () => {
+  return axios("http://localhost:4000/superheroes");
+};
 
 const SuperHeroesPage = () => {
 
-    return (<>
-        hello from SuperHeroesPage
-    </>)
-}
+  const onSuccess = (data) => {
+    console.log("Perform side effect after fetching data", data);
+  };
+  const onError = (error) => {
+    console.log("Perform side effect after encountering an error", error);
+  };
+
+  const {
+    isLoading: isHeroesDataLoading,
+    isFetching: isHeroesDataFetching,
+    data: heroesData,
+    isError: heroesErrorStatus,
+    error: heroesError,
+    refetch,
+  } = useQuery("get-rq-super-heroes", fetchSuperHeroes, {
+    enabled: false,
+    onSuccess,
+    onError,
+  });
+
+  if (isHeroesDataLoading || isHeroesDataFetching) {
+    return <>Loading...</>;
+  }
+  if (heroesErrorStatus) {
+    return <>{heroesError.message}</>;
+  }
+  return (
+    <>
+      <h3>Super Heroes Page</h3>
+      <button onClick={refetch}>Fetch heroes List</button>
+      {heroesData?.data.map((hero) => (
+        <div key={hero.name}>{hero.name}</div>
+      ))}
+    </>
+  );
+};
 
 export default SuperHeroesPage;
